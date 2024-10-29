928f91e2237f2d4412490d9cfae3c9f9bad7eda0
ajit51 <ajitk433@gmail.com>
2024-10-30 01:02:41 +0530

    added binarySearch

---
+++ src/main/java/com/dsa/sprint_boot_dsa/search/Iterative_Binary_Search.java
@@ -0,0 +1,34 @@
+package com.dsa.sprint_boot_dsa.search;
+
+public class Iterative_Binary_Search {
+    public static int iterativeBinarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] sortedArray = {1, 3, 5, 7, 9, 11, 13};
+        int targetElement = 1;
+
+        // Scenario 1: Target is the middle element
+        int foundIndex = Iterative_Binary_Search.iterativeBinarySearch(sortedArray, targetElement);
+        if (foundIndex == -1) {
+            System.out.println(targetElement + " element not found");
+        } else {
+            System.out.println(targetElement + " found at index: " + foundIndex);
+        }
+
+    }
+}
---
+++ src/main/java/com/dsa/sprint_boot_dsa/search/RecursiveBinarySearch.java
@@ -0,0 +1,31 @@
+package com.dsa.sprint_boot_dsa.search;
+
+public class RecursiveBinarySearch {
+
+    private static int binarySearch(int arr[], int target, int left, int righ) {
+        if (left > righ) {
+            return -1;
+        }
+        int mid = left + (righ - left) / 2;
+        if (arr[mid] == target) {
+            return mid;
+        } else if (arr[mid] < target) {
+            return binarySearch(arr, target, mid + 1, righ);
+        } else {
+            return binarySearch(arr, target, left, mid - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] sortedArray = {1, 3, 5, 7, 9, 11, 13};
+        int targetElement = 3;
+
+        // Scenario 1: Target is the middle element
+        int foundIndex = Iterative_Binary_Search.iterativeBinarySearch(sortedArray, targetElement);
+        if (foundIndex == -1) {
+            System.out.println(targetElement + " element not found");
+        } else {
+            System.out.println(targetElement + " found at index: " + foundIndex);
+        }
+    }
+}
